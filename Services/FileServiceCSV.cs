7b3d334bbd301ad0f9ac0ea97d79f0b32b2b693d
Alex Herrera <ao.herrera@outlook.com>
2023-09-23 16:55:33 -0500

    Updated class FileService to FileServiceCSV.

--- Services/FileServiceCSV.cs
+++ Services/FileServiceCSV.cs
@@ -7,7 +7,7 @@ namespace ApplicationTemplate.Services;
 ///     This concrete service and method only exists an example.
 ///     It can either be copied and modified, or deleted.
 /// </summary>
-public class FileService : IFileService
+public class FileServiceCSV : IFileService
 {
     private readonly ILogger<IFileService> _logger;
 
